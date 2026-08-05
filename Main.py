35109e117d7d2248658466342f353f43126eb75b
maksymba0 <maksyma.babiichuka@gmail.com>
2026-08-05 21:57:20 +0200

    added html styling to dashboard

--- Main.py
+++ Main.py
@@ -69,33 +69,6 @@ def home():
 
     return render_template("dashboard.html")
 
-    Gdansk = gdn_scraper.GDN_Scraper("https://www.airport.gdansk.pl/loty/tablica-przylotow")
-    Szczecin = szz_scraper.SZZ_Scraper("https://airport.com.pl/loty/tablica-przylotow-odlotow/")
-    Wroclaw = wro_scraper.WRO_Scraper("https://airport.wroclaw.pl/wp-admin/admin-ajax.php?lang=pl&action=maly_rozklad_lotow")
-    Krakow = krk_scraper.KRK_Scraper("https://krakowairport.pl/pl/pasazer/loty/polaczenia/przyloty")    
-    Poznan = poz_scraper.POZ_Scraper("https://poznanairport.pl/wp-json/api/v1/board/?page=1&phrase=&type=arrivals&day=0&timeFrom=00:00&timeTo=23:59&count=10&lang=pl")
-    WarsawC = waw_scraper.WAW_Scraper("https://lotnisko-chopina.pl/en/arrivals-and-departures/")
-    Bydgoszcz = bzg_scraper.BZG_Scraper("https://plb.pl/wp-admin/admin-ajax.php?action=get_flights_arrivals")
-    Katowice = ktw_scraper.KTW_Scraper("None") #its okay, let it be None
-    Lodz = lcj_scraper.LCJ_Scraper("https://www.lodz-airport.pl/pl")
-    Rzeszow = rze_scraper.RZE_Scraper("https://www.rzeszowairport.pl/pl/pasazer/loty")
-    Radom = rdo_scraper.RDO_Scraper("https://www.lotniskowarszawa-radom.pl/api/search-flight")
-    Olsztyn = szy_scraper.SZY_Scraper("https://mazuryairport.pl/")
-    Lublin = luz_scraper.LUZ_Scraper("https://www.airport.lublin.pl/")
-
-    
-
-
-    result = Lublin.getArrivalsTable()
-    departures = Lublin.getDeparturesAsTable()
-    #Bydgoszcz.getArrivalsTable()
-    #departures = Gdansk.getDeparturesAsTable()
-    #arrivals = Gdansk.getArrivalsTable()
-
-    #f"<h>Airport data: </h> <p>{departures}</p> <p>{arrivals}</p>"
-    output = f"<h>Airport data: </h> <p>{result}</p> <p>{departures}</p>"
-
-    return output 
 @app.route("/api/ping")
 def ping():
     return jsonify({"status":"ok","message":"Flask is alive"})
